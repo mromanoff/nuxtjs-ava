55c5f9882d5b61b013799583b2f9da7925a1dd7e
Michael Romanoff <zelcie@gmail.com>
2018-07-27 20:09:59 -0700

    fix typo

--- test/specs/List.js
+++ test/specs/List.js
@@ -1,25 +1,25 @@
-import test from 'ava'
-import { mount } from '@vue/test-utils'
-import List from '../../components/List'
+import test from 'ava';
+import {mount} from '@vue/test-utils';
+import List from '../../components/List';
 
-let wrapper
+let wrapper;
 
-const list = ['Coffee', 'Tea', 'Snicker', 'Mars']
+const list = ['Coffee', 'Tea', 'Snicker', 'Mars'];
 
 test.beforeEach(() => {
   wrapper = mount(List, {
-    propsData: { list },
-  })
-})
+    propsData: {list},
+  });
+});
 
-test('List.vue shapshow', (t) => {
-  t.snapshot({ html: wrapper.html() })
-})
+test('List.vue snap shot', (t) => {
+  t.snapshot({html: wrapper.html()});
+});
 
 test('List.vue to display list', (t) => {
-  const $items = wrapper.findAll('li').wrappers
+  const $items = wrapper.findAll('li').wrappers;
 
   $items.forEach(($item, index) => {
-    t.is($item.text(), list[index])
-  })
-})
+    t.is($item.text(), list[index]);
+  });
+});
